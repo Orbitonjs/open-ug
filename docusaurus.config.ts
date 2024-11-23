bcaf37a028364579af7cd7d2e61997298acbd991
jim-junior <jimjunior854@gmail.com>
2024-11-23 12:44:01 +0300

    jjj

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,7 +88,16 @@ const config: Config = {
   themeConfig: {
     metadata: [
       { name: "google-adsense-account", content: "ca-pub-4708563499603847" },
-      { name: "monetag", content: "937d7fedcdcf00e8eab3779fc48fc1cc" },
+    ],
+    headTags: [
+      // Declare a <link> preconnect tag
+      {
+        tagName: "meta",
+        attributes: {
+          name: "monetag",
+          content: "937d7fedcdcf00e8eab3779fc48fc1cc",
+        },
+      },
     ],
     // Replace with your project's social card
     image: "img/open-ug.png",
