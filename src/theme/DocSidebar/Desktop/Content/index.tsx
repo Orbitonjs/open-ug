903ec4e92434ea50a88eb72dc8aa8bc1f21fbf83
jim-junior <jimjunior854@gmail.com>
2025-01-08 14:54:56 +0300

    Update digital ocean referal badge

--- src/theme/DocSidebar/Desktop/Content/index.tsx
+++ src/theme/DocSidebar/Desktop/Content/index.tsx
@@ -56,7 +56,10 @@ export default function DocSidebarDesktopContent({
           }}
         >
           <a href="https://www.digitalocean.com/?refcode=ad96e8b378d5&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge">
-            <img src="https://web-platforms.sfo2.cdn.digitaloceanspaces.com/WWW/Badge%201.svg" />
+            <img
+              src="https://web-platforms.sfo2.cdn.digitaloceanspaces.com/WWW/Badge%201.svg"
+              alt="DigitalOcean Referral Badge"
+            />
           </a>
         </li>
       </ul>
